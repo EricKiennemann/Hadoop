3cb802c53febd4701356d597dd4b7f8dce481bcf
Eric Kiennemann <63780741+EricKiennemann@users.noreply.github.com>
2020-10-07 08:03:26 +0200

    Create consumer.sh

---
+++ Kafka/consumer.sh
@@ -0,0 +1,5 @@
+export brokers="kfk-brk-1.au.adaltas.cloud:6667,kfk-brk-2.au.adaltas.cloud:6667,kfk-brk-3.au.adaltas.cloud:6667"
+
+export zk_quorum="zoo-1.au.adaltas.cloud:2181,zoo-2.au.adaltas.cloud:2181,kfk-brk-3.au.adaltas.cloud:2181/kafka"
+
+/usr/hdp/current/kafka-broker/bin/kafka-console-consumer.sh --bootstrap-server $brokers --consumer-property security.protocol=SASL_PLAINTEXT --topic dsti_2020_fall_1
